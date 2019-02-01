a3c101a52ce1e87ad32ab97921ba56ac1204de39
Felipe Carvalho <felipecavalcanti100@gmail.com>
2019-02-01 14:51:05 -0300

    Adding check for start game

--- src/App.vue
+++ src/App.vue
@@ -118,7 +118,7 @@ export default {
   },
   methods: {
     gameStart: function() {
-      if(this.playerPokemon.url === '' && this.enemyPokemon.url === '' ) {
+      if(this.playerPokemon.url === '' || this.enemyPokemon.url === '' ) {
         alert('Select Pokemons for the battle!')
       } else {
         this.gameIsRunning = true
