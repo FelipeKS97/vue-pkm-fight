490c15fb8f48df4d9c909b60cad453abf98c6847
Felipe Carvalho <felipecavalcanti100@gmail.com>
2019-01-31 21:35:22 -0300

    Introducing Vue project structure, vue-cli version 3 used

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,11 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Monster Slayer</title>
-    <script src="https://npmcdn.com/vue/dist/vue.js"></script>
-    <link rel="stylesheet" href="css/foundation.min.css">
-    <link rel="stylesheet" href="css/app.css">
-</head>
-<body>
-<div id="app">
+<template>
+  <div id="app">
     <section class="row">
         <div class="small-6 columns">
             <h1 class="text-center">YOU</h1>
             <div class="healthbar">
                 <div class="healthbar text-center" style="background-color: green; margin: 0; color: white;">
-
+                  {{ playerHealth }}
                 </div>
             </div>
         </div>
@@ -21,22 +13,22 @@
             <h1 class="text-center">MONSTER</h1>
             <div class="healthbar">
                 <div class="healthbar text-center" style="background-color: green; margin: 0; color: white;">
-
+                 {{ enemyHealth }}
                 </div>
             </div>
         </div>
     </section>
     <section class="row controls">
         <div class="small-12 columns">
-            <button id="start-game">START NEW GAME</button>
+            <button class="button" id="start-game">START NEW GAME</button>
         </div>
     </section>
     <section class="row controls">
         <div class="small-12 columns">
-            <button id="attack">ATTACK</button>
-            <button id="special-attack">SPECIAL ATTACK</button>
-            <button id="heal">HEAL</button>
-            <button id="give-up">GIVE UP</button>
+            <button class="button" id="attack">ATTACK</button>
+            <button class="button" id="special-attack">SPECIAL ATTACK</button>
+            <button class="button" id="heal">HEAL</button>
+            <button class="button" id="give-up">GIVE UP</button>
         </div>
     </section>
     <section class="row log">
@@ -48,6 +40,24 @@
             </ul>
         </div>
     </section>
-</div>
-</body>
-</html>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "app",
+  data: function() { 
+    return {
+      playerHealth: 100,
+      enemyHealth: 100,
+      gameIsRunning: false
+    }
+  }
+};
+</script>
+
+<style>
+ @import '../css/app.css';
+ @import '../css/foundation.min.css';
+</style>
